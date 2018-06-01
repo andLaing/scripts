bbeb8af1233e382ce1ffa899f403ac94d5b73270
Andrew Laing <a.b.r.laing@gmail.com>
2018-06-01 15:54:34 +0200

    debugging

--- vetoedPDFs.py
+++ vetoedPDFs.py
@@ -142,6 +142,7 @@ def ring_veto(cwf, n_ring, z_veto, hit_pos, xy):
 
     ## Forces out of the histo range (or not if you have weird ranges)
     print(cwf.shape, veto_indcs[0].shape, z_veto.shape, np.invert(z_veto).shape)
+    print(cwf[veto_indcs[0]])
     cwf[veto_indcs[0], np.invert(z_veto)] = -100000
     return cwf
     
