ca4cb95f652467f77d5b700156f16f8bf4184e0f
Andrew Laing <a.b.r.laing@gmail.com>
2018-06-14 15:49:32 +0200

    eroCounter

--- zeroCount.py
+++ zeroCount.py
@@ -22,20 +22,22 @@ def count_zeros():
                     
                 sens_ids = np.fromiter((ch[1] for ch in data_in.root.Sensors.DataSiPM), np.int)
                 evts     = np.fromiter((ev[0] for ev in data_in.root.Run.events), np.int)
+
+                buffer_len = len(sipmrwf[0][0])
                 print('file: '+ifile)
                 for evt_no, evtrwf in zip(evts, sipmrwf):
                     out = str(evt_no)
                     
                     indx = np.argwhere(sens_ids == 5010)[0][0]
-                    prop_zero_5010 = 1 - np.count_nonzero(evtrwf[indx]) / 80
+                    prop_zero_5010 = 1 - np.count_nonzero(evtrwf[indx]) / buffer_len
                     out += ' ch 5010 '+str(prop_zero_5010)
                     
                     indx = np.argwhere(sens_ids == 10010)[0][0]
-                    prop_zero_10010 = 1 - np.count_nonzero(evtrwf[indx]) / 80
+                    prop_zero_10010 = 1 - np.count_nonzero(evtrwf[indx]) / buffer_len
                     out += ' ch 10010 '+str(prop_zero_10010)
                     
                     indx = np.argwhere(sens_ids == 11010)[0][0]
-                    prop_zero_11010 = 1 - np.count_nonzero(evtrwf[indx]) / 80
+                    prop_zero_11010 = 1 - np.count_nonzero(evtrwf[indx]) / buffer_len
                     out += ' ch 11010 '+str(prop_zero_11010)+'\n'
                     
                     out_dat.write(out)
